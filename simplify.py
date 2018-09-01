3a58d9ad7c06975a4fe80c759ef96048a47f236f
Hussein Bakri <hussein_bakri@yahoo.com>
2018-09-01 21:27:30 +0100

    Update simplify.py

--- simplify.py
+++ simplify.py
@@ -72,7 +72,25 @@ print ('TexturesFlag:', TexturesFlag)
 #Do the models have textures?
 #TexturesFlag=True
 
-simplified_meshScript = mlx.FilterScript(file_in=original_mesh, file_out=simplified_mesh,
+#Check the input mesh number of faces (so that we do not decimate to a higher number of faces than original mesh)
+MetricsMeshDictionary = {}
+MetricsMeshDictionary = mlx.files.measure_topology(original_mesh)
+#print (MetricsMeshDictionary)
+print('\n Number of faces of original mesh is: ' + str(MetricsMeshDictionary['face_num'] )) 
+
+if(MetricsMeshDictionary['face_num'] <= Num_Of_Faces):
+    #exit the script and print a message about it
+    print("\n SORRY your decimated mesh can not have higher number of faces that the input mesh.....")
+    print("\n ......................................................................................")
+    sys.exit()
+
+#Creating a folder named as the Number of faces: named '150000'
+print('\n Creating a folder to store the decimated model ...........')
+if not os.path.exists(str(Num_Of_Faces)):
+    os.makedirs(str(Num_Of_Faces))
+
+
+simplified_meshScript = mlx.FilterScript(file_in=original_mesh, file_out=str(Num_Of_Faces) + '/' + simplified_mesh,
                                          ml_version='2016.12')  # Create FilterScript object
 
 mlx.remesh.simplify(simplified_meshScript, texture=TexturesFlag, faces=Num_Of_Faces,
@@ -80,6 +98,24 @@ mlx.remesh.simplify(simplified_meshScript, texture=TexturesFlag, faces=Num_Of_Fa
                     boundary_weight=1.0, preserve_normal=True,
                     optimal_placement=True, planar_quadric=True,
                     selected=False, extra_tex_coord_weight=1.0)
-print('\n Beginning the process of Decimation ...')
+print('\n Beginning the process of Decimation ...........')
+
 simplified_meshScript.run_script()  # Run the script
+os.chdir(str(Num_Of_Faces))
 print('\n Process of Decimation Finished ...')
+print('\n Copying textures (PNG and JPEG) into the folder of decimated model....')
+
+#go back to parent directory so we can copy the textures to the 3D Model folder
+os.chdir('..')
+
+#Now checking for textures in the folder of the input mesh.... (plz change if needed)
+allfilelist= os.listdir('.')
+
+for Afile in allfilelist[:]: 
+    if not(Afile.endswith(".png") or Afile.endswith(".PNG") or Afile.endswith(".jpg") or Afile.endswith(".JPG")):
+        allfilelist.remove(Afile)
+print('\n Found the LIST of images in PNG and JPEG (textures): ')
+print(allfilelist)
+
+for file in allfilelist:
+    shutil.copy(file, str(Num_Of_Faces))
